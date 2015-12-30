af2683e9e7fb8d8804907a15908447f5aaac69cb
Michael Chapman <submariner100@gmail.com>
2015-12-30 19:16:33 +0000

    Initial Commit

---
+++ pokemon-by-Chappy-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokemon-by-Chappy-App
+//
+//  Created by Macbook on 30/12/2015.
+//  Copyright © 2015 Chappy-App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
